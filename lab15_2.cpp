66dfdbea1f3665b7645283114d3dad368641023d
Fluke <adisorn.sjr@gmil.com>
2023-01-17 17:33:05 +0700

    casdsa

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 const int N = 5;
 
-void inputMatrix(double [][N]);
+void inputMatrix(double	[][N]);
 
 void findLocalMax(const double [][N], bool [][N]);
 
@@ -19,3 +19,43 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double	A[][N]){
+	for(int i=0; i<N; i++){
+		cout << "Row " << i+1 << ": ";
+		for(int j = 0; j<N;j++){
+			cin >> A[i][j];
+		}
+	}
+}
+
+void showMatrix(const bool B[][N]){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			cout << B[i][j] << " ";
+		}
+		cout << "\n";
+ 	}
+}
+
+void findLocalMax(const double A[][N], bool B[][N]){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			B[i][j] = 0;
+
+		}
+	}
+	for(int i = 1;i<N-1;i++){
+		for(int j = 1;j<N-1;j++){
+			if(A[i][j] >= A[i-1][j] && A[i][j] >= A[i+1][j] && A[i][j] >= A[i][j-1] && A[i][j] >= A[i][j+1]){
+				B[i][j] = 1;
+			}else{
+				B[i][j] = 0;
+			}
+
+		}
+		
+	}	
+	
+}
+
